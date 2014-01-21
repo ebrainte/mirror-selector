41654ea88c1710e7ed2c6fe99c6cf8d17f088be1
Ariel <ebrainte@gmail.com>
2014-01-21 13:31:34 -0200

    Some improvements

--- mirror.sh
+++ mirror.sh
@@ -7,6 +7,17 @@ if [[ $# -ne 1 ]]; then
 	exit 1
 fi
 
+
+
+echo "Checking Ubuntu Version"
+UB_VERSION=$(lsb_release -c | awk '{print $2}')
+if [[ $UB_VERSION != saucy ]] && [[ $UB_VERSION != raring ]] && [[ $UB_VERSION != precise ]]; then
+	echo "You need Ubuntu to use this script"
+	exit 1
+fi
+
+
+
 if [[ $1 == office ]]; then
 
 	count=$(ping -c 1 mirror.despegar.it | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
@@ -16,25 +27,30 @@ if [[ $1 == office ]]; then
 		exit 1
 	fi
 
-	if [[ ! -f /etc/apt/sources.internet ]]; then
-		mv /etc/apt/sources.list /etc/apt/sources.internet
+	if [[ ! -f /etc/apt/sources.original ]]; then
+		mv /etc/apt/sources.list /etc/apt/sources.original
 	fi
 
+	if [[ ! -f /etc/apt/sources.office ]]; then
 	echo "Getting new sources.list"
-	wget http://10.254.130.200/sources.list	-O /etc/apt/sources.local
+	wget http://mirror.despegar.it/sources.txt	-O /etc/apt/sources.office
+	sed -i "s/\$VERSION/$UB_VERSION/" /etc/apt/sources.office
+	fi
+	
 	mv /etc/apt/sources.list /etc/apt/sources.list.bak
-	cp /etc/apt/sources.local /etc/apt/sources.list
+	cp /etc/apt/sources.office /etc/apt/sources.list
 	echo "Success!"
 fi
 
 if [[ $1 == original ]]; then
 
-        if [[ ! -f /etc/apt/sources.internet ]];then
-                cp /etc/apt/sources.list /etc/apt/sources.internet
+        if [[ ! -f /etc/apt/sources.original ]];then
+                echo "You have the original sources.list"
+                exit 1
         fi
 
 	mv /etc/apt/sources.list /etc/apt/sources.list.bak
-	cp /etc/apt/sources.internet /etc/apt/sources.list
+	cp /etc/apt/sources.original /etc/apt/sources.list
 	echo "Success!"
 
 exit 0
